60db51fe52ef78fa0d9002c86c873fa62959ae58
Miro Svrtan <miro@mirosvrtan.me>
2013-04-06 11:09:16 +0200

    Using db for user identification

--- blog/protected/components/UserIdentity.php
+++ blog/protected/components/UserIdentity.php
@@ -7,27 +7,32 @@
  */
 class UserIdentity extends CUserIdentity
 {
-	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
-	 * @return boolean whether authentication succeeds.
-	 */
-	public function authenticate()
-	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
-	}
+
+    private $_id;
+
+    /**
+     * Authenticates a user.
+     * The example implementation makes sure if the username and password
+     * are both 'demo'.
+     * In practical applications, this should be changed to authenticate
+     * against some persistent user identity storage (e.g. database).
+     * @return boolean whether authentication succeeds.
+     */
+    public function authenticate()
+    {
+        $user = User::model()->find('username=:username', array('username' => $this->username));
+
+        if ($user === null) {
+            $this->errorCode = self::ERROR_USERNAME_INVALID;
+        } elseif (!$user->validatePassword($this->password)) {
+            $this->errorCode = self::ERROR_PASSWORD_INVALID;
+        } else {
+
+            $this->_id       = $user->id;
+            $this->username  = $user->username;
+            $this->errorCode = self::ERROR_NONE;
+        }
+        return $this->errorCode == self::ERROR_NONE;
+
+    }
 }
