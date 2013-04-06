e5c74e5745d3cd74d33c1c0027ca262fad99c205
Miro Svrtan <miro@mirosvrtan.me>
2013-04-06 13:09:26 +0200

    Added drop down list for status in post form

--- blog/protected/views/post/_form.php
+++ blog/protected/views/post/_form.php
@@ -36,7 +36,7 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'status'); ?>
-		<?php echo $form->textField($model,'status'); ?>
+		<?php echo $form->dropDownList($model,'status', Lookup::items('PostStatus')); ?>
 		<?php echo $form->error($model,'status'); ?>
 	</div>
 
