4a7f7b3071550c9f0154fb3d3a58a2db9a57fe3f
Miro Svrtan <miro@mirosvrtan.me>
2013-04-06 11:39:55 +0200

    Added string2array() && array2string() to Tag model

--- blog/protected/models/Tag.php
+++ blog/protected/models/Tag.php
@@ -87,4 +87,14 @@ class Tag extends CActiveRecord
 			'criteria'=>$criteria,
 		));
 	}
+
+    public static function string2array($tags)
+    {
+        return preg_split('/\s*,\s*/',trim($tags),-1,PREG_SPLIT_NO_EMPTY);
+    }
+
+    public static function array2string($tags)
+    {
+        return implode(', ',$tags);
+    }
 }
